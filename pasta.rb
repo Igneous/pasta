9599f065d6918cd4e4dcf36a38e119425f32a860
John Wolfe <bjw@onwav.com>
2011-06-15 03:00:49 -0500

    Initial commit

--- pasta.rb
+++ pasta.rb
@@ -6,6 +6,7 @@ require 'data_mapper'
 require 'dm-migrations'
 require 'albino'
 require 'net/http'
+require 'uv'
 
 # Set path to sqlite3 database file
 set :public, File.dirname(__FILE__) + '/static'
@@ -18,6 +19,8 @@ class Paste
   property :id, Serial
   property :title, String
   property :body, Text
+  property :bodywlines, Text
+  property :rawbody, Text
   property :author, String
   property :created_at, DateTime
 end
@@ -36,12 +39,41 @@ get '/:id' do |id|
   haml :paste
 end
 
+get '/lnos/:id' do |id|
+  @paste = Paste.get(Integer(id))
+  haml :pastewlines
+end
+
+get '/raw/:id' do |id|
+  @paste = Paste.get(Integer(id))
+  haml :raw
+end
+
+get '/download/:id' do |id|
+  @paste = Paste.get(Integer(id))
+  if @paste.title != "Untitled"
+    @fname = @paste.title
+  else
+    @fname = 'paste' + id + '.txt'
+  end
+
+  # There's probably a better way to do this..
+  File.open(File.dirname(__FILE__) + "/static/rraw/#{id}", 'w') {|f| f.write(@paste.rawbody) }
+  send_file File.dirname(__FILE__) + "static/rraw/#{id}", :type => :txt, :filename => @fname
+end
+
 post '/' do
-  request = Net::HTTP.post_form(URI.parse('http://pygments.appspot.com/'),
-                                {'lang'=>params[:language], 
-                                 'code'=>params[:body]}
-                               )
-  body = request.body
+
+  # This is a nasty hack :(
+  if params[:language] == "bash"
+    @lang = "shell-unix-generic"
+  else
+    @lang = params[:language]
+  end
+
+  rawbody = params[:body]
+  body = Uv.parse(params[:body], "xhtml", @lang, false, "eiffel")
+  bodywlines = Uv.parse(params[:body], "xhtml", @lang, true, "eiffel")
 
   if params[:title].empty?
     title = "Unknown"
@@ -58,7 +90,9 @@ post '/' do
   new_paste = Paste.create(
                            :title => title,
                            :author => author,
+                           :rawbody => rawbody,
                            :body => body,
+                           :bodywlines => bodywlines,
                            :created_at => Time.now
                            )
   redirect "/#{new_paste[:id]}"
